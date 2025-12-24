afbbb6dd53d708dea1b5767e1b8791b993f84eb6
Evgeniy Dzhurov <edj@fastmail.fm>
2025-12-24 17:38:24 +0200

    Add node_id and config node parameters

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,18 @@ mod consensus;
 mod node;
 mod rpc;
 mod server;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(name = "raft-rs", about = "Raft node")]
+struct Args {
+    #[arg(long, short)]
+    node_id: i32,
+    #[arg(long, short)]
+    config: String,
+}
+
 fn main() {
-    // TODO: logging backend initialization
-    println!("Hello, Raft!");
+    let args = Args::parse();
+    println!("Starting node {} with config {}", args.node_id, args.config);
 }
--- src/node.rs
+++ src/node.rs
@@ -1,6 +1,8 @@
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
+use crate::server::Server;
+
 #[derive(Debug, Serialize, Deserialize, Clone)]
 struct Peer {
     id: i32,
@@ -12,32 +14,24 @@ struct ClusterConfig {
     peers: Vec<Peer>,
 }
 
-#[tarpc::service]
-struct RpcClient;
-
-#[tarpc::service]
-struct RpcServer;
-
 struct Node {
     id: i32,
-    rpc_server: RpcServer,
-    rpc_clients: HashMap<i32, RpcClient>,
 }
 
 impl Server for Node {
     fn call_request_vote(
         &self,
         peer: i32,
-        req: &crate::messages::RequestVote,
-    ) -> Result<crate::messages::RequestVoteReply, crate::server::RequestError> {
+        req: &crate::rpc::RequestVote,
+    ) -> Result<crate::rpc::RequestVoteReply, crate::server::RequestError> {
         todo!()
     }
 
     fn call_append_entries(
         &self,
         peer: i32,
-        req: &crate::messages::AppendEntries,
-    ) -> Result<crate::messages::AppendEntriesReply, crate::server::RequestError> {
+        req: &crate::rpc::AppendEntries,
+    ) -> Result<crate::rpc::AppendEntriesReply, crate::server::RequestError> {
         todo!()
     }
 }
